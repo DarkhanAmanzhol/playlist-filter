a3b4d2948543e65fcfc14da8c9d6aa71a12566b9
Darkhan Amanzhol <darhan25amanzhol@gmail.com>
2022-10-19 02:18:59 +0600

    client side is builded

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,13 @@ const app = express();
 app.use(express.json());
 app.use("/api", api);
 
+// Using a `public` folder as a starting point
+console.log(path.join(__dirname, "..", "public"));
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.get("/.*/", (req, res) =>
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+);
+
 const PORT = process.env.PORT | 5000;
 
 app.listen(PORT, () => {
