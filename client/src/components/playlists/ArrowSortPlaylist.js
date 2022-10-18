6eaac5d37c10ac5e1f0d0697594362257273f12c
Darkhan Amanzhol <darhan25amanzhol@gmail.com>
2022-10-18 16:05:08 +0600

    created arrow sort playlist component

---
+++ client/src/components/playlists/ArrowSortPlaylist.js
@@ -0,0 +1,29 @@
+import React from "react";
+import triangleDownSvg from "../../images/triangle-down.svg";
+import triangleUpSvg from "../../images/triangle-up.svg";
+import doubleTriangle from "../../images/triangles-up-down.svg";
+
+function ArrowSortPlaylist({ nameColumn, type, prevType, setType }) {
+  return (
+    <div
+      className="arrows"
+      onClick={() => {
+        setType({ name: nameColumn, order: type.order * -1 });
+        prevType.current = type.name;
+      }}
+    >
+      <img
+        src={
+          type.name === nameColumn
+            ? type.order === 1
+              ? triangleDownSvg
+              : triangleUpSvg
+            : doubleTriangle
+        }
+        alt="triangle-svg"
+      />
+    </div>
+  );
+}
+
+export default ArrowSortPlaylist;
