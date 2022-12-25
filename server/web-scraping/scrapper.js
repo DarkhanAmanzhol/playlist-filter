b9e850fe8d9f3e7a77f739f69e82b92937347161
Darkhan Amanzhol <darhan25amanzhol@gmail.com>
2022-12-26 03:45:44 +0600

    scrapped data from wikipedia and saved into json file

---
+++ server/web-scraping/scrapper.js
@@ -0,0 +1,45 @@
+const axios = require("axios");
+const jsdom = require("jsdom");
+const fs = require("fs");
+const path = require("path");
+
+const { JSDOM } = jsdom;
+
+const url =
+  "https://en.wikipedia.org/wiki/List_of_downloadable_songs_for_the_Rock_Band_series";
+
+async function getMusics() {
+  const response = await axios.get(url);
+
+  const dom = new JSDOM(response.data);
+
+  const musics = [];
+
+  dom.window.document.querySelectorAll(".wikitable td:nth-child(1)").forEach(
+    (node, index) =>
+      (musics[index] = {
+        song: node.textContent.trim().replaceAll('"', ""),
+      })
+  );
+
+  dom.window.document
+    .querySelectorAll(".wikitable td:nth-child(2)")
+    .forEach((node, index) => (musics[index].singer = node.textContent.trim()));
+
+  dom.window.document
+    .querySelectorAll(".wikitable td:nth-child(3)")
+    .forEach(
+      (node, index) => (musics[index].year = parseInt(node.textContent.trim()))
+    );
+
+  dom.window.document
+    .querySelectorAll(".wikitable td:nth-child(4)")
+    .forEach((node, index) => (musics[index].genre = node.textContent.trim()));
+
+  fs.writeFileSync(
+    path.join(__dirname, "..", "src", "database", "musics.json"),
+    JSON.stringify(musics)
+  );
+}
+
+getMusics();
